6fa9feebe9f61ababe90892296149e3a4296fef5
ErickGomezUCA <00300723@uca.edu.sv>
2024-05-25 10:13:43 -0600

    feature: Create CEO to be treated as a Composite too

--- SegundoParcialPOO_GomezHernandez_00300723/src/CEO.java
+++ SegundoParcialPOO_GomezHernandez_00300723/src/CEO.java
@@ -1,2 +1,35 @@
-public class CEO {
+import java.util.Vector;
+
+// Composite 1: CEO
+public class CEO extends Person implements DepartmentComponent {
+    // Common Composite attributes
+    // Composite content
+    private Vector<DepartmentComponent> content;
+
+    public CEO(String name, int age, int id) {
+        super(name, age, id);
+        this.content = new Vector<DepartmentComponent>();
+    }
+
+    public void add(DepartmentComponent component) {
+        content.add(component);
+    }
+
+    public void remove(DepartmentComponent component) {
+        content.remove(component);
+    }
+
+    public Vector<DepartmentComponent> getContent() {
+        return content;
+    }
+
+    // Implemented execute method
+    @Override
+    public void showInformation() {
+        System.out.println("[CEO " + getName() + "]: Consists of the following departments:");
+        for (DepartmentComponent element : content) {
+            // Delegate printing information to content of composite
+            element.showInformation();
+        }
+    }
 }
