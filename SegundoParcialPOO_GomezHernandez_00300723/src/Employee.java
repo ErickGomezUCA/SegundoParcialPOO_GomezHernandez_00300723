ec245e2a209b9a2b2468c5f617c8595c3e2b9cf1
ErickGomezUCA <00300723@uca.edu.sv>
2024-05-25 10:01:01 -0600

    feat: Remove Person abstract class to include attritubes directly to Employee class

--- SegundoParcialPOO_GomezHernandez_00300723/src/Employee.java
+++ SegundoParcialPOO_GomezHernandez_00300723/src/Employee.java
@@ -1,10 +1,52 @@
-public class Employee extends Person implements DepartmentComponent {
+public class Employee implements DepartmentComponent {
+
+    private String name;
+    private int age;
+    private int id;
+    private String workPosition;
+
     public Employee(String name, int age, int id, String workPosition) {
-        super(name, age, id, workPosition);
+        this.name = name;
+        this.age = age;
+        this.id = id;
+        this.workPosition = workPosition;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWorkPosition() {
+        return workPosition;
+    }
+
+    public void setWorkPosition(String workPosition) {
+        this.workPosition = workPosition;
+    }
+
 
     @Override
     public void showWorks() {
-        System.out.println("[Employee " + getName() + "] I am an employee, I do work!");
+        System.out.println("[Employee " + getName() + "] I am " + age + " years old. My work position is: " + workPosition);
     }
 }
